177bae98ecee8aa365c97eca70267452e61f566a
cokoyoh <charlesokoyoh@gmail.com>
2017-11-28 18:41:02 +0000

    Update action of CRUD completed

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,8 +4,9 @@
         <div class="row">
             <div class="col-lg-10 col-md-10 col-lg-offset-1 col-md-offset-1">
                 <h4>Edit Blog</h4>
-                <form method="PUT" action="/posts/{{$post->id}}">
+                <form method="post" action="/posts/{{$post->id}}">
                     {{csrf_field()}}
+                    {{method_field('PUT')}}
                     <div class="form-group">
                         <label for="email">Title of Blog</label>
                         <input type="text" class="form-control" id="title" name="title" value="{{$post->title}}">
