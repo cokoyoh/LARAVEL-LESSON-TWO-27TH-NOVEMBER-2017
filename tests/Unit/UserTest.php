2db03c379099920e0719382a838aeee654e58e3c
cokoyoh <charlesokoyoh@gmail.com>
2017-11-30 17:50:24 +0300

    Dusk installed and initial test run

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+//    public function testLoginURL() {
+//        $response = $this->get('/login');
+//        $response = assertStatus(200);
+//    }
+}
