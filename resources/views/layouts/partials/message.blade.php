9eb71cb61207b5310c481445e3cfa7427927f328
cokoyoh <charlesokoyoh@gmail.com>
2017-11-30 09:05:21 +0300

    partial message edited

--- resources/views/layouts/partials/message.blade.php
+++ resources/views/layouts/partials/message.blade.php
@@ -1,3 +1,3 @@
 @if(session()->has('message'))
-    <h3 class="alert alert-success">{{session()->get('message')}}</h3>
+    <h4 class="alert alert-success">{{session()->get('message')}}</h4>
 @endif
