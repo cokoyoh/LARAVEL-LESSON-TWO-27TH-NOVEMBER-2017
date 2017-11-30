d8cd56a5cb1c713edf4ea0c693b7b1330f4b20a5
cokoyoh <charlesokoyoh@gmail.com>
2017-11-30 04:37:43 +0300

    Changed the cache driver to database and generated a cache table

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'file'),
+    //change the default from file to database
+    'default' => env('CACHE_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
